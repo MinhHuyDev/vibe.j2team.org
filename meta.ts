6ba5db533a2a8f47f4e6a3321c4875e0c5b2e922
Nguyễn Minh Huy <96736064+MinhHuyDev@users.noreply.github.com>
2026-04-07 21:58:36 +0700

    Add meta information for Gold Market page

---
+++ meta.ts
@@ -0,0 +1,8 @@
+const meta: PageMeta = {
+  name: 'Gold Market - Giá vàng trong nước',
+  description: 'Cập nhật giá vàng theo phút với 70 cơ sở khác nhau',
+  author: 'Nguyễn Minh Huy',
+  facebook: 'https://www.facebook.com/zMinhHuyDev',
+}
+
+export default meta
